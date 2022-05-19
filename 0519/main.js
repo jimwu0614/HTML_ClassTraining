c9381fc9369e33f8eb5492eaa95f232da0bcce32
chiachun wu <a0283137@gmail.com>
2022-05-19 12:19:06 +0800

    JS選擇器

--- 0519/main.js
+++ 0519/main.js
@@ -2,6 +2,7 @@
 const myForm = document.getElementById("myForm")
 const num1Input = document.getElementById("num1")
 const num2Input = document.getElementById("num2")
+const optionInput = document.getElementById("option")
 const resultP = document.getElementById("result")
 
 console.log(myForm);
@@ -16,10 +17,39 @@ myForm.addEventListener('submit', function (e) {
     console.log(num2Input.value)
     let num1 =parseInt(num1Input.value)
     let num2 =parseInt(num2Input.value)
-    let sum = num1 + num2
+    let option = optionInput.value;
+
+
+    let resultNum = 0;
+    switch (option) {
+        case "+":
+            resultNum = num1 + num2 ;        
+            break;
+        case "-":
+            resultNum = num1 - num2 ;        
+            break;
+        case "*":
+            resultNum = num1 * num2 ;        
+            break;
+        case "/":
+            resultNum = num1 / num2 ;        
+            break;
     
-    console.log(sum)
-    console.log(num1 + "+" + num2 + "=" + sum)
+        default:
+            break;
+    }
+        // let sum = num1 + num2;
+        console.log(resultNum);
+        // ` 分號 esc下面 數字1左邊
+        // console.log("您好");
+        myString1 = `${num1} ${option} ${num2} = ${resultNum}`;
+        // myString2 = num1 + " + " + num2 + " = " + sum;
+        // console.log(myString1);
+        // console.log(myString2);
+        resultP.innerText = myString1;
+    
+            //submit get value
+        
 })
 
 
